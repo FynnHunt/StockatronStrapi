f00f8ce2e71ecd8ee2a3abda5b87ed7eb9a10b9a
Fynn <fynnhunt@hotmail.co.uk>
2020-02-09 13:31:06 +0000

    Made db file persistent

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,6 @@ COPY package.json .
 RUN npm install --quiet
 
 COPY . .
+VOLUME ["/sqlitedata"]
+
+CMD "/launch.sh"
---
+++ launch.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Waiting 3 seconds for deployer interrupt ..."
+sleep 3
+
+echo "Starting ..."
+npm run develop
