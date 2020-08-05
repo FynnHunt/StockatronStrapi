f5add25792d625b828e50dde8ffe93a82160ec32
Fynn <fynnhunt@hotmail.co.uk>
2020-08-05 15:29:00 +0100

    Added redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -1,4 +1,4 @@
 sudo docker stop stockatron-strapi
 git pull
 sudo docker build -t stockatron-strapi .
-sudo docker run -d -it -p 1337:1337 --name stockatron-strapi -v $(pwd)/sqlitedata:/sqlitedata stockatron-strapi
+sudo docker run -d -it --rm -p 1337:1337 --name stockatron-strapi -v $(pwd)/sqlitedata:/sqlitedata stockatron-strapi
