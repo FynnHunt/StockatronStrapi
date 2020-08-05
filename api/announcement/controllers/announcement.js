7d55a2ca9a3f97e21285621084be7d02d59a849c
Fynn <fynnhunt@hotmail.co.uk>
2020-08-05 15:23:29 +0100

    Added get news endpoint

--- api/announcement/controllers/announcement.js
+++ api/announcement/controllers/announcement.js
@@ -5,16 +5,30 @@
  * to customize this controller
  */
 
+const getMovement = movement => {
+    let numMovement = movement.replace('%', '');
+    if (numMovement[0] === '-') numMovement = -Math.abs(Number(numMovement));
+    else numMovement = Number(numMovement);
+    return numMovement;
+}
+
 module.exports = {
-    async getList(ctx) {
+    async getAnnList(ctx) {
         const entities = await strapi.services.announcement.find(ctx.query);
         const annData = entities.map(ann => {
-            let movement = ann.movement.replace('%', '');
-            if (movement[0] === '-') movement = -Math.abs(Number(movement));
-            else movement = Number(movement);
+            const movement = getMovement(ann.movement);
             return { Announcement: ann.announcement, Movement: movement };
-        })
-    
+        });   
+        return annData;
+    },
+    async getNewsList(ctx) {
+        const entities = await strapi.services.announcement.find(ctx.query);
+        const annData = entities.map(ann => {
+            if (ann.news) {
+                const movement = getMovement(ann.movement);
+                return { Announcement: ann.news, Movement: movement };
+            } else return null;
+        }).filter(n => n);
         return annData;
     },
 };
