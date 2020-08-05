cb0ae125a48eb7e7b9da29c8b89953e276e3b84d
Fynn <fynnhunt@hotmail.co.uk>
2020-08-05 14:45:51 +0100

    Added getlist endpoint

--- api/announcement/controllers/announcement.js
+++ api/announcement/controllers/announcement.js
@@ -5,4 +5,16 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async getList(ctx) {
+        const entities = await strapi.services.announcement.find(ctx.query);
+        const annData = entities.map(ann => {
+            let movement = ann.movement.replace('%', '');
+            if (movement[0] === '-') movement = -Math.abs(Number(movement));
+            else movement = Number(movement);
+            return { Announcement: ann.announcement, Movement: movement };
+        })
+    
+        return annData;
+    },
+};
